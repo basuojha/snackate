4164f969818b95d254a9e27d457329542c7b6e38
Basu Ojha <basuojha25@gmail.com>
2024-05-09 18:52:07 +0530

    add uat changes to gallery and add empty caption support

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'I am H1'),
-    React.createElement('h2', {}, 'I am H2')
+    React.createElement('h1', {}, 'Welcome to React 🚀'),
+    React.createElement('h2', {}, 'I am H2 hahahahah')
   ]),
   React.createElement('div', { id: 'child2' }, [
     React.createElement('h1', {}, 'I am H1'),
