38d6aa24e35ef21d07f632f9e8825a0065d54a03
Basu Ojha <basuojha25@gmail.com>
2024-08-30 21:30:01 +0530

    add restaurant card list

---
+++ src/constants/restaurantList.js
@@ -0,0 +1,211 @@
+export const restaurantList = [
+  {
+    name: 'The Gourmet Kitchen',
+    timeForDelivery: 30,
+    imgSrc: 'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg',
+    rating: 4.5,
+    priceForTwo: 40,
+    cuisine: ['Italian', 'Continental'],
+    address: '123 Main St, City Center',
+    discount: 10
+  },
+  {
+    name: 'Spicy Delight',
+    timeForDelivery: 25,
+    imgSrc: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg',
+    rating: 4.2,
+    priceForTwo: 35,
+    cuisine: ['Indian', 'Thai'],
+    address: '78 Curry Lane, Little India',
+    discount: 15
+  },
+  {
+    name: 'Sushi Samba',
+    timeForDelivery: 45,
+    imgSrc: 'https://images.pexels.com/photos/357756/pexels-photo-357756.jpeg',
+    rating: 4.8,
+    priceForTwo: 50,
+    cuisine: ['Japanese', 'Sushi'],
+    address: '987 Sushi Ave, Uptown',
+    discount: 12
+  },
+  {
+    name: 'Burger Heaven',
+    timeForDelivery: 20,
+    imgSrc:
+      'https://images.pexels.com/photos/1639569/pexels-photo-1639569.jpeg',
+    rating: 4.1,
+    priceForTwo: 20,
+    cuisine: ['American', 'Fast Food'],
+    address: '321 Burger Blvd, Midtown',
+    discount: 5
+  },
+  {
+    name: 'Pasta Fresca',
+    timeForDelivery: 35,
+    imgSrc:
+      'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg',
+    rating: 4.7,
+    priceForTwo: 45,
+    cuisine: ['Italian', 'Mediterranean'],
+    address: '654 Olive St, Historic District',
+    discount: 8
+  },
+  {
+    name: 'Dragon Wok',
+    timeForDelivery: 40,
+    imgSrc:
+      'https://images.pexels.com/photos/1442397/pexels-photo-1442397.jpeg',
+    rating: 4.3,
+    priceForTwo: 30,
+    cuisine: ['Chinese', 'Asian'],
+    address: '312 Dragon Rd, Chinatown',
+    discount: 10
+  },
+  {
+    name: 'Taco Fiesta',
+    timeForDelivery: 15,
+    imgSrc: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg',
+    rating: 4.0,
+    priceForTwo: 25,
+    cuisine: ['Mexican', 'Tex-Mex'],
+    address: '789 Fiesta Blvd, West End',
+    discount: 7
+  },
+  {
+    name: 'Curry House',
+    timeForDelivery: 50,
+    imgSrc: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg',
+    rating: 4.4,
+    priceForTwo: 40,
+    cuisine: ['Indian', 'Vegetarian'],
+    address: '100 Spice St, Little India',
+    discount: 20
+  },
+  {
+    name: 'Pizza Palace',
+    timeForDelivery: 20,
+    imgSrc:
+      'https://images.pexels.com/photos/1579734/pexels-photo-1579734.jpeg',
+    rating: 4.6,
+    priceForTwo: 30,
+    cuisine: ['Italian', 'Fast Food'],
+    address: '432 Mozzarella Ln, City Center',
+    discount: 10
+  },
+  {
+    name: 'Noodle House',
+    timeForDelivery: 30,
+    imgSrc:
+      'https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg',
+    rating: 4.5,
+    priceForTwo: 35,
+    cuisine: ['Chinese', 'Asian'],
+    address: '678 Noodle Ave, East Side',
+    discount: 12
+  },
+  {
+    name: 'Steak Station',
+    timeForDelivery: 45,
+    imgSrc: 'https://images.pexels.com/photos/952377/pexels-photo-952377.jpeg',
+    rating: 4.9,
+    priceForTwo: 60,
+    cuisine: ['American', 'Steakhouse'],
+    address: '159 Grill Rd, Downtown',
+    discount: 5
+  },
+  {
+    name: 'Vegan Vibes',
+    timeForDelivery: 20,
+    imgSrc:
+      'https://images.pexels.com/photos/1640775/pexels-photo-1640775.jpeg',
+    rating: 4.3,
+    priceForTwo: 28,
+    cuisine: ['Vegan', 'Healthy'],
+    address: '951 Plant St, Green District',
+    discount: 18
+  },
+  {
+    name: 'Gourmet Burgers',
+    timeForDelivery: 15,
+    imgSrc:
+      'https://images.pexels.com/photos/1639569/pexels-photo-1639569.jpeg',
+    rating: 4.4,
+    priceForTwo: 32,
+    cuisine: ['American', 'Fast Food'],
+    address: '852 Patty Ln, Midtown',
+    discount: 6
+  },
+  {
+    name: 'Thai Spice',
+    timeForDelivery: 35,
+    imgSrc:
+      'https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg',
+    rating: 4.7,
+    priceForTwo: 38,
+    cuisine: ['Thai', 'Asian'],
+    address: '200 Spice Ave, Downtown',
+    discount: 15
+  },
+  {
+    name: 'BBQ Central',
+    timeForDelivery: 50,
+    imgSrc: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg',
+    rating: 4.5,
+    priceForTwo: 55,
+    cuisine: ['American', 'BBQ'],
+    address: '333 BBQ Blvd, West Side',
+    discount: 10
+  },
+  {
+    name: 'Ramen Rave',
+    timeForDelivery: 25,
+    imgSrc:
+      'https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg',
+    rating: 4.6,
+    priceForTwo: 35,
+    cuisine: ['Japanese', 'Ramen'],
+    address: '204 Ramen St, Uptown',
+    discount: 8
+  },
+  {
+    name: 'Mediterranean Magic',
+    timeForDelivery: 40,
+    imgSrc: 'https://images.pexels.com/photos/58721/pexels-photo-58721.jpeg',
+    rating: 4.8,
+    priceForTwo: 45,
+    cuisine: ['Mediterranean', 'Greek'],
+    address: '650 Mediterranean Ave, Historic District',
+    discount: 9
+  },
+  {
+    name: 'Pancake House',
+    timeForDelivery: 20,
+    imgSrc: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg',
+    rating: 4.1,
+    priceForTwo: 25,
+    cuisine: ['American', 'Breakfast'],
+    address: '310 Maple Syrup Blvd, South Side',
+    discount: 5
+  },
+  {
+    name: 'French Delight',
+    timeForDelivery: 35,
+    imgSrc: 'https://images.pexels.com/photos/58721/pexels-photo-58721.jpeg',
+    rating: 4.9,
+    priceForTwo: 60,
+    cuisine: ['French', 'European'],
+    address: '909 Eclair St, Downtown',
+    discount: 12
+  },
+  {
+    name: 'Street Tacos',
+    timeForDelivery: 15,
+    imgSrc: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg',
+    rating: 4.2,
+    priceForTwo: 20,
+    cuisine: ['Mexican', 'Street Food'],
+    address: '78 Salsa St, West End',
+    discount: 7
+  }
+]
